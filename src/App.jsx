ee4c5b849df547980eacd28a09d02371a04cd91a
Bilal4m <bilal_4m@live.com>
2023-11-15 16:14:57 +0500

    FunctionalityAdded

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Display from "./components/Display";
+import styles from "./App.module.css";
+import ButtonsContainer from "./components/ButtonsContainer";
+import { useState } from "react";
+function App() {
+  const [calVal, setCalVal] = useState("");
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newValue = calVal + buttonText;
+      setCalVal(newValue);
+    }
+  };
+  return (
+    <div className={styles.calculator}>
+      <Display displayValue={calVal}></Display>
+      <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
+    </div>
+  );
+}
+export default App;
